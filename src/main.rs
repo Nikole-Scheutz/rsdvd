c2763d0bd63558d1b7a28e114d10a6804906130c
Nikole-Scheutz <nikolai@jinxan.se>
2023-05-12 18:50:55 +0200

    add and implement struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::stdout;
+use std::{thread, time};
 
 use crossterm::{
     execute, Result,
@@ -11,27 +12,56 @@ use crossterm::{
      x: u16,
      y: u16,
  }
- 
- 
- fn dvd_printer(pos: Position, logo: [String; 7]) -> Result<()> {
-     execute!(
-         stdout(),
-         Clear(ClearType::All),
-         MoveTo(pos.x,pos.y),
-         )?;
 
-     for line in logo {
-         execute!(
-             stdout(),
-             SavePosition,
-             Print(line.to_string()),
-             MoveToNextLine(0),
-             )?;
-     }
- 
-     Ok(())
- }
+impl Position {
+    fn new(x: u16, y: u16) -> Position {
+        Position {x,y}
+    }
+}
 
+struct Dvd {
+    pos: Position,
+    logo: [String; 7],
+}
+
+impl Dvd {
+    pub fn new(pos: Position, logo: [String; 7]) -> Dvd {
+        Dvd {pos,logo}
+    }
+
+    pub fn printer(&self) -> Result<()> {
+        execute!(
+            stdout(),
+            Clear(ClearType::All),
+            MoveTo(self.pos.x,self.pos.y),
+            )?;
+
+        for line in &self.logo {
+            execute!(
+                stdout(),
+                SavePosition,
+                Print(line.to_string()),
+                MoveToNextLine(0),
+                )?;
+        }
+        Ok(())
+    }
+
+    pub fn change_position(&mut self, x_loops: u8) -> Result<()> {
+        for x_change in 0..x_loops {
+            let new_position = Position {x: self.pos.x + x_change as u16, y: self.pos.y};
+            self.printer()?;
+            self.pos.x = new_position.x;
+            wait_ms(500);
+        }
+        Ok(())
+    }
+}
+
+fn wait_ms(ms: u64) {
+    thread::sleep(time::Duration::from_millis(ms));
+}
+     
  fn main() -> Result<()> {
      let dvd_logo: [String; 7] = [
              "⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⡀".to_string(),
@@ -44,8 +74,12 @@ use crossterm::{
          ];
      
      let position = Position {x: 0, y: 0};
+
+     let dvd = Dvd::new(position,dvd_logo);
  
-     dvd_printer(position, dvd_logo).unwrap();
- 
+     dvd.printer().unwrap();
+
+    // change_position(10, &mut position, &dvd_logo)?;
+
      Ok(())
  }
