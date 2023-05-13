097d3bed0ce4585af4fba7c4e0be57460ff35c52
Nikole-Scheutz <nikolai@jinxan.se>
2023-05-13 12:05:39 +0200

    move code out of main.rs

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+use::std::process;
+use std::io::stdout;
+use std::{thread, time};
+
+use crossterm::{
+    execute, Result,
+    style::Print,
+    cursor::{MoveTo, SavePosition, MoveDown, MoveToNextLine, RestorePosition, Hide, Show},
+    terminal::{Clear,ClearType},
+};
+
+pub struct Position {
+    x: u16,
+    y: u16,
+}
+
+impl Position {
+    pub fn new(x: u16, y: u16) -> Position {
+        Position {x,y}
+    }
+
+    fn change(&mut self, x: i16, y: i16) {
+        self.x = self.x.wrapping_add_signed(x);
+        self.y = self.y.wrapping_add_signed(y);
+    }
+}
+
+pub struct Dvd {
+    pos: Position,
+    logo: Vec<String>,
+}
+
+impl Dvd {
+    pub fn new() -> Dvd {
+    let logo: Vec<String> = vec![
+        "⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⡀".to_string(),
+        "⠀⢠⣿⣿⡿⠀⠀⠈⢹⣿⣿⡿⣿⣿⣇⠀⣠⣿⣿⠟⣽⣿⣿⠇⠀⠀⢹⣿⣿⣿".to_string(),
+        "⠀⢸⣿⣿⡇⠀⢀⣠⣾⣿⡿⠃⢹⣿⣿⣶⣿⡿⠋⢰⣿⣿⡿⠀⠀⣠⣼⣿⣿⠏".to_string(),
+        "⠀⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⠀⠀⢿⣿⣿⠏⠀⠀⢸⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀".to_string(),
+        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣸⣟⣁⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀".to_string(),
+        "⣠⣴⣶⣾⣿⣿⣻⡟⣻⣿⢻⣿⡟⣛⢻⣿⡟⣛⣿⡿⣛⣛⢻⣿⣿⣶⣦⣄⡀⠀".to_string(),
+        "⠉⠛⠻⠿⠿⠿⠷⣼⣿⣿⣼⣿⣧⣭⣼⣿⣧⣭⣿⣿⣬⡭⠾⠿⠿⠿⠛⠉".to_string()
+    ];
+    let pos = Position::new(0,0);
+        Dvd {pos,logo}
+    }
+
+    pub fn print(&self) -> Result<()> {
+        execute!(
+            stdout(),
+            Clear(ClearType::All),
+            Hide,
+            MoveTo(self.pos.x,self.pos.y),
+            SavePosition,
+            )?;
+
+        for line in &self.logo {
+            execute!(
+                stdout(),
+                SavePosition,
+                Print(line.to_string()),
+                RestorePosition,
+                MoveDown(0),
+                SavePosition,
+                )?;
+        }
+        Ok(())
+    }
+
+    pub fn change_position(&mut self, x_loops: u8) -> Result<()> {
+        for _i in 0..x_loops {
+            self.pos.change(1,1);
+            self.print()?;
+            wait_ms(500);
+        }
+        Ok(())
+    }
+
+    fn restore_cursor(&self) {
+        execute!(
+            stdout(),
+            Show,
+            MoveToNextLine(0),
+            ).unwrap_or_else(|err| {
+            println!("Problem restoring cursor: {err}");
+            process::exit(1);
+        });
+    }
+}
+
+pub fn wait_ms(ms: u64) {
+    thread::sleep(time::Duration::from_millis(ms));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,12 @@
-use std::io::stdout;
-use std::{thread, time};
 use std::process::exit;
 
-use crossterm::{
-    execute, Result,
-    style::Print,
-    cursor::{MoveTo, SavePosition, MoveDown, MoveToNextLine, RestorePosition, Hide, Show},
-    terminal::{Clear,ClearType},
-};
-
-struct Position {
-    x: u16,
-    y: u16,
-}
-
-impl Position {
-    fn new(x: u16, y: u16) -> Position {
-        Position {x,y}
-    }
-
-    fn change(&mut self, x: i16, y: i16) {
-        self.x = self.x.wrapping_add_signed(x);
-        self.y = self.y.wrapping_add_signed(y);
-    }
-}
-
-struct Dvd {
-    pos: Position,
-    logo: Vec<String>,
-}
-
-impl Dvd {
-    pub fn new(pos: Position, logo: Vec<String>) -> Dvd {
-        Dvd {pos,logo}
-    }
-
-    pub fn print(&self) -> Result<()> {
-        execute!(
-            stdout(),
-            Clear(ClearType::All),
-            Hide,
-            MoveTo(self.pos.x,self.pos.y),
-            SavePosition,
-            )?;
-
-        for line in &self.logo {
-            execute!(
-                stdout(),
-                SavePosition,
-                Print(line.to_string()),
-                RestorePosition,
-                MoveDown(0),
-                SavePosition,
-                )?;
-        }
-        Ok(())
-    }
-
-    pub fn change_position(&mut self, x_loops: u8) -> Result<()> {
-        for _i in 0..x_loops {
-            self.pos.change(1,1);
-            self.print()?;
-            wait_ms(500);
-        }
-        Ok(())
-    }
-
-    pub fn restore_cursor(&self) {
-        execute!(
-            stdout(),
-            Show,
-            MoveToNextLine(0),
-            ).unwrap_or_else(|err| {
-            println!("Problem restoring cursor: {err}");
-            exit(1);
-        });
-    }
-}
-
-fn wait_ms(ms: u64) {
-    thread::sleep(time::Duration::from_millis(ms));
-}
+use rsdvd::Dvd;
 
 fn main() {
-    let dvd_logo: Vec<String> = vec![
-        "⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⡀".to_string(),
-        "⠀⢠⣿⣿⡿⠀⠀⠈⢹⣿⣿⡿⣿⣿⣇⠀⣠⣿⣿⠟⣽⣿⣿⠇⠀⠀⢹⣿⣿⣿".to_string(),
-        "⠀⢸⣿⣿⡇⠀⢀⣠⣾⣿⡿⠃⢹⣿⣿⣶⣿⡿⠋⢰⣿⣿⡿⠀⠀⣠⣼⣿⣿⠏".to_string(),
-        "⠀⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⠀⠀⢿⣿⣿⠏⠀⠀⢸⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀".to_string(),
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣸⣟⣁⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀".to_string(),
-        "⣠⣴⣶⣾⣿⣿⣻⡟⣻⣿⢻⣿⡟⣛⢻⣿⡟⣛⣿⡿⣛⣛⢻⣿⣿⣶⣦⣄⡀⠀".to_string(),
-        "⠉⠛⠻⠿⠿⠿⠷⣼⣿⣿⣼⣿⣧⣭⣼⣿⣧⣭⣿⣿⣬⡭⠾⠿⠿⠿⠛⠉".to_string()
-    ];
-    let position = Position::new(0,0);
-
-    let mut dvd = Dvd::new(position,dvd_logo);
+    let mut dvd = Dvd::new();
 
     if let Err(e) = dvd.change_position(5) {
         println!("Application error: {e}");
         exit(1);
     }
-
-    dvd.restore_cursor();
 }
